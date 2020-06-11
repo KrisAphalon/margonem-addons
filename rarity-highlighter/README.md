d36ab97f66c9b38a26e70ac4172c72ac8addef71
Krzysztof K <Krzysztof.Kacz@interia.pl>
2020-06-11 04:16:04 +0200

    Added rarity-highlighter

---
+++ rarity-highlighter/README.md
@@ -0,0 +1,5 @@
+# Rarity highlighter
+
+This was a test for creating APNG images measuring limits for how smooth they can be.
+It turned out successfully, however chromium browser has problems with displaying more than few frames of APNG images,
+so ultimately there is a webp version provided as well.
---
+++ rarity-highlighter/script.js
@@ -0,0 +1,7 @@
+!function ()
+{
+    const css = ".itemHighlighter{background-image: url(https://cdn.jsdelivr.net/gh/KrisAphalon/margonem-addons/rarity-highlighter/rarity-highlights.webp);opacity:1}"
+    const style = document.createElement("style")
+    style.appendChild(document.createTextNode(css))
+    document.head.appendChild(style)
+}()
